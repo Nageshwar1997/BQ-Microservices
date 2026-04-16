b8a54dbbea116526f2500a38a49026e2e214c8ae
Nageshwar <nageshpawarpatil@gmail.com>
2026-04-16 17:00:24 +0530

    Update package dependencies and add nodemon for development; refactor database connection and logging middleware

--- user/src/configs/index.ts
+++ user/src/configs/index.ts
@@ -1,4 +1,14 @@
+import { LoggerMiddleware } from "@beautinique/be-middlewares";
+import { connection } from "mongoose";
+
 export const databaseConfigs = {
   uri: "",
   isDev: true,
 };
+
+export const isDbConnected = () => connection.readyState === 1;
+
+export const { requestLog, errorLog, logger } = LoggerMiddleware.createLogger({
+  logDir: "logs",
+  level: "info",
+});
--- user/src/index.ts
+++ user/src/index.ts
@@ -2,33 +2,29 @@ import "dotenv/config";
 import path from "path";
 import express, { Request, Response } from "express";
 import { parse } from "qs";
-// import { mailService, redisService } from "./services";
-// import { connectDB } from "./configs";
 import { envs } from "./envs";
 import { router } from "./router";
-import { connectToMongoDB } from "@beautinique/be-configs";
-import { databaseConfigs } from "./configs";
+import { connectToDB } from "@beautinique/be-configs";
+import {
+  databaseConfigs,
+  errorLog,
+  isDbConnected,
+  requestLog,
+} from "./configs";
 import {
   CorsMiddleware,
   DatabaseMiddleware,
+  RequestMiddleware,
   ResponseMiddleware,
 } from "@beautinique/be-middlewares";
 import { ORIGINS } from "./constants";
-// import { router } from "./router";
-// import {
-//   CorsMiddleware,
-//   DatabaseMiddleware,
-//   LoggerMiddleware,
-//   RequestMiddleware,
-//   ResponseMiddleware,
-// } from "./middlewares";
 
 const app = express();
 
 // ----------------- MIDDLEWARES ORDER -----------------
 
 // 1. Assign requestId first (for tracing logs)
-// app.use(RequestMiddleware.requestId);
+app.use(RequestMiddleware.requestId);
 
 // 2. Body parsers & static files
 app.use(express.json());
@@ -37,12 +33,12 @@ app.use(express.static(path.resolve("public")));
 app.set("query parser", (str: string) => parse(str));
 
 // 3. Logger (logs all requests)
-// app.use(LoggerMiddleware.requestLog);
+app.use(requestLog);
 
 // 4. Custom middlewares
-app.use(ResponseMiddleware.successResponse);
-app.use(CorsMiddleware.createCorsMiddleware({ origins: ORIGINS }));
-app.use(DatabaseMiddleware.createDbConnectionMiddleware());
+app.use(ResponseMiddleware.success);
+app.use(CorsMiddleware.checkOrigin({ origins: ORIGINS }));
+app.use(DatabaseMiddleware.checkConnection(isDbConnected));
 
 // ----------------- ROUTES -----------------
 // Home Route
@@ -54,13 +50,13 @@ app.get("/", (_: Request, res: Response) =>
 app.use("/api", router);
 
 // ----------------- ERROR HANDLING -----------------
-// app.use(ResponseMiddleware.notFoundResponse);
-// app.use(LoggerMiddleware.errorLog);
-// app.use(ResponseMiddleware.errorResponse);
+app.use(ResponseMiddleware.notFound);
+app.use(errorLog);
+app.use(ResponseMiddleware.error);
 
 (async () => {
   try {
-    await connectToMongoDB(databaseConfigs);
+    await connectToDB(databaseConfigs);
     // await Promise.all([redisService.connect(), mailService.checkConnection()]);
 
     app.listen(envs.port, () => {
