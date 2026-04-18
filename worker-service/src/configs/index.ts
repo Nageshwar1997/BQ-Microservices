12518cd43390856c18359ba7ff3b85d80c9af318
Nageshwar <nageshpawarpatil@gmail.com>
2026-04-18 19:33:26 +0530

    Name Changed from Email Service to Worker Service

--- worker-service/src/configs/index.ts
+++ worker-service/src/configs/index.ts
@@ -2,7 +2,7 @@ import { LoggerMiddleware } from '@beautinique/be-middlewares';
 import { envs } from '@/envs';
 
 export const { errorLogger, logger, requestLogger } = LoggerMiddleware.createLogger({
-  serviceName: 'Email-Service',
+  serviceName: 'Worker-Service',
   logDir: 'logs',
   level: envs.is_dev ? 'debug' : 'info',
 });
