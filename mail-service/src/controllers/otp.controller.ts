df189f4537818f829fd7619d9d4d00b470e4d6b1
Nageshwar <nageshpawarpatil@gmail.com>
2026-04-19 11:52:27 +0530

    otp send controller added

--- mail-service/src/controllers/otp.controller.ts
+++ mail-service/src/controllers/otp.controller.ts
@@ -1,1 +1,9 @@
-export const ctrl = 'asdsa';
+import { mailService } from '@/services';
+import type { TSendOtpMail } from '@beautinique/be-zod';
+import type { Request, Response } from 'express';
+
+export const otpController = async (req: Request, res: Response) => {
+  const { email, otp } = req.body as TSendOtpMail;
+  await mailService.sendOtp(email, otp);
+  res.success(200, 'OTP sent successfully');
+};
--- mail-service/src/routes/index.ts
+++ mail-service/src/routes/index.ts
@@ -1,3 +1,13 @@
+import { otpController } from '@/controllers';
+import { RequestMiddleware, ResponseMiddleware, ZodMiddleware } from '@beautinique/be-middlewares';
+import { sendOtpMailSchema } from '@beautinique/be-zod';
 import { Router } from 'express';
 
 export const router = Router();
+
+router.post(
+  '/send-otp',
+  RequestMiddleware.emptyRequest({ body: true }),
+  ZodMiddleware.validateSchema(sendOtpMailSchema),
+  ResponseMiddleware.tryCatch(otpController),
+);
