4723816ef5c778d429f449a76099c831cc773dbe
Nageshwar <nageshpawarpatil@gmail.com>
2026-04-20 12:26:39 +0530

    Refactor environment variable handling: update frontend and backend URLs, streamline configuration

--- mail-service/src/envs/index.ts
+++ mail-service/src/envs/index.ts
@@ -1,29 +1,33 @@
 const {
   // A
+
+  ADMIN_DEV_URL,
+  ADMIN_PROD_URL,
+
   // B
-  BACKEND_DEV_URL,
-  BACKEND_PROD_URL,
   // C
+
+  CLIENT_DEV_URL,
+  CLIENT_PROD_URL,
+
   // D
   // E
   // F
-  FRONTEND_DEV_CLIENT_URL,
-  FRONTEND_DEV_ADMIN_URL,
-  FRONTEND_DEV_MASTER_URL,
-  FRONTEND_DEV_PUBLIC_URL_1,
-  FRONTEND_DEV_PUBLIC_URL_2,
-
-  FRONTEND_PROD_CLIENT_URL,
-  FRONTEND_PROD_ADMIN_URL,
-  FRONTEND_PROD_MASTER_URL,
   // G
+
+  GATEWAY_DEV_URL,
+  GATEWAY_PROD_URL,
+
   // H
   // I
+
   IS_DEV,
+
   // J
   // K
   // L
   // M
+
   MAIL_HOST,
   MAIL_PORT,
   MAIL_USER,
@@ -32,22 +36,37 @@ const {
 
   MAIL_SERVICE_DEV_URL,
   MAIL_SERVICE_PROD_URL,
+
+  MASTER_DEV_URL,
+  MASTER_PROD_URL,
+
   // N
   // O
   // P
+
   PORT,
+
+  PUBLIC_DEV_URL_1,
+  PUBLIC_DEV_URL_2,
+
   // Q
   // R
   // S
+
   SERVICE_NAME,
+
   // T
   // U
+
   USER_SERVICE_DEV_URL,
   USER_SERVICE_PROD_URL,
+
   // V
   // W
+
   WORKER_SERVICE_DEV_URL,
   WORKER_SERVICE_PROD_URL,
+
   // X
   // Y
   // Z
@@ -65,11 +84,14 @@ export const envs = {
   // G
   // H
   // I
+
   is_dev,
+
   // J
   // K
   // L
   // M
+
   mail: {
     host: MAIL_HOST,
     port: Number(MAIL_PORT),
@@ -77,31 +99,38 @@ export const envs = {
     pass: MAIL_PASS,
     from: MAIL_FROM,
   },
+
   // N
   // O
   // P
+
   port: Number(PORT),
+
   // Q
   // R
   // S
+
   service_name: SERVICE_NAME,
+
   // T
   // U
+
   url: {
-    gateway: is_dev ? BACKEND_DEV_URL : BACKEND_PROD_URL,
+    gateway: is_dev ? GATEWAY_DEV_URL : GATEWAY_PROD_URL,
     service: {
       mail: is_dev ? MAIL_SERVICE_DEV_URL : MAIL_SERVICE_PROD_URL,
       user: is_dev ? USER_SERVICE_DEV_URL : USER_SERVICE_PROD_URL,
       worker: is_dev ? WORKER_SERVICE_DEV_URL : WORKER_SERVICE_PROD_URL,
     },
     frontend: {
-      client: is_dev ? FRONTEND_DEV_CLIENT_URL : FRONTEND_PROD_CLIENT_URL,
-      admin: is_dev ? FRONTEND_DEV_ADMIN_URL : FRONTEND_PROD_ADMIN_URL,
-      master: is_dev ? FRONTEND_DEV_MASTER_URL : FRONTEND_PROD_MASTER_URL,
-      public1: FRONTEND_DEV_PUBLIC_URL_1,
-      public2: FRONTEND_DEV_PUBLIC_URL_2,
+      client: is_dev ? CLIENT_DEV_URL : CLIENT_PROD_URL,
+      admin: is_dev ? ADMIN_DEV_URL : ADMIN_PROD_URL,
+      master: is_dev ? MASTER_DEV_URL : MASTER_PROD_URL,
+      public1: PUBLIC_DEV_URL_1,
+      public2: PUBLIC_DEV_URL_2,
     },
   },
+
   // V
   // W
   // X
