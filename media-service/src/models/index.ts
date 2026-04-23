ea6875cc1caf705a33cab66053b578fd4af23cf9
Nageshwar <nageshpawarpatil@gmail.com>
2026-04-23 18:34:53 +0530

    Media Model added

---
+++ media-service/src/models/index.ts
@@ -0,0 +1,25 @@
+import { Schema, Types, model } from 'mongoose';
+
+const MediaSchema = new Schema(
+  {
+    publicId: { type: String, required: true, unique: true, index: true },
+    url: { type: String },
+    resourceType: { type: String, enum: ['image', 'video'], required: true },
+    uploadedBy: { type: Types.ObjectId },
+    deletedBy: { type: Types.ObjectId },
+    relatedTo: {
+      service: { type: String, enum: ['user-service'], required: true },
+      entity: { type: String },
+      entityId: { type: String },
+    },
+    expiresAt: { type: Date, index: true },
+    status: { type: String, enum: ['pending', 'used', 'deleted'], default: 'pending', index: true },
+    metadata: { type: Schema.Types.Mixed },
+    isDeleted: { type: Boolean, default: false, index: true },
+    isUsed: { type: Boolean, default: false, index: true },
+  },
+  { timestamps: true, versionKey: false },
+);
+MediaSchema.index({ status: 1, expiresAt: 1 });
+
+export const Media = model('Media', MediaSchema);
