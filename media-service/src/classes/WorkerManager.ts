8bdac8e9d5d93c1af66877b0f8e0c501b8bd61dd
Nageshwar <nageshpawarpatil@gmail.com>
2026-05-10 22:56:50 +0530

    feat(worker-manager): implement WorkerManager class for media processing jobs

---
+++ media-service/src/classes/WorkerManager.ts
@@ -0,0 +1,42 @@
+import { bullWorker } from '@beautinique/be-jobs';
+
+import { envs } from '../envs';
+import { cloudinary } from './Cloudinary';
+
+class WorkerManager {
+  /* ---------------- CONNECT ---------------- */
+  private connect() {
+    bullWorker.connect(envs.redis.queue);
+  }
+
+  /* ---------------- RUN WORKERS ---------------- */
+  private runWorkers() {
+    // During Upload Fail Handlers
+    bullWorker.createWorker({
+      queueName: 'media-queue',
+      jobName: 'single-media-remove',
+      handler: async (job) => void (await cloudinary.removeSingle(job.data)),
+    });
+
+    bullWorker.createWorker({
+      queueName: 'media-queue',
+      jobName: 'multiple-media-remove',
+      handler: async (job) => void (await cloudinary.removeMultiple(job.data)),
+    });
+  }
+
+  /* ---------------- START ---------------- */
+
+  public start() {
+    this.connect();
+    this.runWorkers();
+  }
+
+  /* ---------------- CLOSE ---------------- */
+
+  public async stop() {
+    await bullWorker.closeAll();
+  }
+}
+
+export const workerManager = new WorkerManager();
