97bd7a1fe6b8dd797f1c76450e345cac26013d4d
Nageshwar <nageshpawarpatil@gmail.com>
2026-05-12 13:10:46 +0530

    refactor(media-service): remove bullmq dependencies and related code

--- media-service/src/constants/index.ts
+++ media-service/src/constants/index.ts
@@ -1,6 +1,4 @@
 import type { TMediaResource } from '@beautinique/be-constants';
-import type { ConnectionOptions } from 'bullmq';
-import { envs } from '../envs';
 
 export const MIME_TO_FORMAT: Record<TMediaResource, Record<string, string>> = {
   image: {
@@ -19,25 +17,4 @@ export const METHODS_AND_PATHS = {
     single: { method: 'post', path: '/single' },
     multiple: { method: 'post', path: '/multiple' },
   },
-} as const;
-
-export const QUEUE_CONFIGS: ConnectionOptions = envs.redis.job;
-
-export const QUEUE_AND_JOB_NAMES = {
-  'email-queue': ['send-otp'],
-  'media-queue': [
-    // Cloudinary Job Names
-    'single-media-remove',
-    'multiple-media-remove',
-    'single-media-remove-if-unused',
-    'multiple-media-remove-if-unused',
-
-    // Database Job Names
-    'mark-as-unused-single-media',
-    'mark-as-unused-multiple-media',
-    'mark-as-used-single-media',
-    'mark-as-used-single-media',
-    'mark-as-deleted-multiple-media',
-    'mark-as-deleted-multiple-media',
-  ],
 } as const;
--- media-service/src/types/index.ts
+++ media-service/src/types/index.ts
@@ -1,8 +1,6 @@
 import type { TMediaResource, TMediaStatus, TRole, TService } from '@beautinique/be-constants';
-import type { JobsOptions } from 'bullmq';
 import type { Request } from 'express';
 import type { Types } from 'mongoose';
-import type { QUEUE_AND_JOB_NAMES } from '../constants';
 
 export type TId = Types.ObjectId;
 export interface IId {
@@ -17,15 +15,6 @@ export interface ITimestamp {
   createdAt: Date;
   updatedAt: Date;
 }
-
-type TQueue = typeof QUEUE_AND_JOB_NAMES;
-export type TQueueKey = keyof TQueue;
-type TQueueJobName = { [K in TQueueKey]: { queueName: K; jobName: TQueue[K][number] } }[TQueueKey];
-
-export type IQueueJob<TData = unknown> = TQueueJobName & {
-  data: TData;
-  options?: JobsOptions;
-};
 
 interface IResource {
   resourceType: TMediaResource;
