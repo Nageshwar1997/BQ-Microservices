23b4f3d67dc6b1c93aa6302af80266d40a326e89
Nageshwar <nageshpawarpatil@gmail.com>
2026-05-16 21:06:19 +0530

    refactor(category-controller): remove redundant category retrieval methods and streamline category fetching

--- product-service/src/controllers/category/getCategory.ts
+++ product-service/src/controllers/category/getCategory.ts
@@ -1,12 +1,6 @@
 import type { Request, Response } from 'express';
 import { redisCache } from '../../classes';
 
-export const getAllCategories = async (_req: Request, res: Response) => {
-  const categories = await redisCache.getAllCategories();
-
-  res.success(200, 'Categories fetched successfully', { categories });
-};
-
 export const getCategoriesByParentLevel = async (req: Request, res: Response) => {
   const parentId = req.query.parentId?.toString();
   const level = Number(req.query.level);
@@ -26,36 +20,6 @@ export const getCategoriesByParentLevel = async (req: Request, res: Response) =>
     // Level 2 & 3 → parent check required
     return category.parent?.toString() === parentId;
   });
-
-  res.success(200, 'Categories fetched successfully', { categories });
-};
-
-export const getL1Categories = async (_req: Request, res: Response) => {
-  const allCategories = await redisCache.getAllCategories();
-
-  const categories = allCategories.filter((category) => category.level === 1);
-  res.success(200, 'Categories fetched successfully', { categories });
-};
-
-export const getL2Categories = async (req: Request, res: Response) => {
-  const parentId = req.params.parentId as string;
-
-  const allCategories = await redisCache.getAllCategories();
-
-  const categories = allCategories.filter(
-    (category) => category.level === 2 && category.parent?.toString() === parentId,
-  );
-  res.success(200, 'Categories fetched successfully', { categories });
-};
-
-export const getL3Categories = async (req: Request, res: Response) => {
-  const parentId = req.params.parentId;
-
-  const allCategories = await redisCache.getAllCategories();
-
-  const categories = allCategories.filter(
-    (category) => category.level === 3 && category.parent?.toString() === parentId,
-  );
 
   res.success(200, 'Categories fetched successfully', { categories });
 };
