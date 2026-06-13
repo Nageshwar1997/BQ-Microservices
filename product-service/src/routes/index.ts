0662db88abe2d13fe51f8bb4157e7f99a579488c
Nageshwar <nageshpawarpatil@gmail.com>
2026-06-13 15:57:39 +0530

    router updated

--- product-service/src/routes/index.ts
+++ product-service/src/routes/index.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { METHODS_AND_PATHS } from '../constants';
 import { categoryRouter } from './category.routes';
+import { productRouter } from './product.routes';
 
 export const router = Router();
 
 const { category, product } = METHODS_AND_PATHS;
 
 router.use(category.base, categoryRouter);
-router.use(product.base, categoryRouter);
+router.use(product.base, productRouter);
