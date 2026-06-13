ed4fc17d3694f3cebe6a9e34df2aaa1ac7881933
Nageshwar <nageshpawarpatil@gmail.com>
2026-06-13 12:57:26 +0530

    Product routes file added

--- product-service/src/constants/index.ts
+++ product-service/src/constants/index.ts
@@ -25,6 +25,17 @@ export const METHODS_AND_PATHS = {
       byHierarchy: { method: METHOD_MAP.GET, path: '/by-hierarchy' },
     },
   },
+  product: {
+    base: '/product',
+    draft: {
+      base: '/draft',
+      save: { method: METHOD_MAP.POST, path: '/' }, // For upload new Product as draft
+      get: { method: METHOD_MAP.GET, path: '/' }, // For get existing draft Product
+      remove: { method: METHOD_MAP.DELETE, path: '/' }, // For remove existing draft
+      update: { method: METHOD_MAP.POST, path: '/renew' }, // For already published product and seller again made some changes
+    },
+    createDraft: { method: METHOD_MAP.POST, path: '/create-draft' },
+  },
 } as const;
 
 export const PRODUCT_STATUS_MAP = {
--- product-service/src/routes/index.ts
+++ product-service/src/routes/index.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { METHODS_AND_PATHS } from '../constants';
-import { categoryRouter } from './category.route';
+import { categoryRouter } from './category.routes';
 
 export const router = Router();
 
-const { category } = METHODS_AND_PATHS;
+const { category, product } = METHODS_AND_PATHS;
 
 router.use(category.base, categoryRouter);
+router.use(product.base, categoryRouter);
