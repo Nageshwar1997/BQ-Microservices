0dbabbc5225a0064ee06967fee9e43e551bf48b1
Nageshwar <nageshpawarpatil@gmail.com>
2026-07-12 16:00:45 +0530

    refactor: remove null and undefined check functions from utils

--- media-service/src/utils/index.ts
+++ media-service/src/utils/index.ts
@@ -6,15 +6,6 @@ import { Types } from 'mongoose';
 
 import type { TId } from '../types/index.js';
 
-/* ========== NULL CHECK FUNCTION ========== */
-export const isNull = (value: unknown): value is null => value === null;
-/* ========== NULL CHECK FUNCTION ========== */
-export const isUndefined = (value: unknown): value is undefined => value === undefined;
-
-export const isNullOrUndefined = (value: unknown): value is null | undefined => {
-  return isNull(value) || isUndefined(value);
-};
-
 /* ========== OBJECT ID CONVERTER FUNCTION ========== */
 
 export const toObjectId = (id: string): TId => {
