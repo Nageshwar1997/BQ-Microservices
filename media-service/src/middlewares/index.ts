d8677b51586b91e46490411f227d36c75b88fcb5
Nageshwar <nageshpawarpatil@gmail.com>
2026-07-30 23:52:19 +0530

    Fix: build issue

--- media-service/src/middlewares/index.ts
+++ media-service/src/middlewares/index.ts
@@ -1,24 +1,25 @@
 import { AuthenticationError, AuthorizationError } from '@beautinique/backend-classes';
+import { getObjId } from '@beautinique/backend-mongoose';
 import { HEADERS_MAP, USER_ROLE_MAP } from '@beautinique/shared-constants';
 import type { TUserRole } from '@beautinique/shared-types';
 import type { NextFunction, Request, Response } from 'express';
 
 export const authenticate = (req: Request, _res: Response, next: NextFunction) => {
-  const userId = req.get(HEADERS_MAP.userId) ?? '';
+  const userId = req.get(HEADERS_MAP.userId);
   const userRole = (req.get(HEADERS_MAP.userRole) ?? USER_ROLE_MAP.USER) as TUserRole;
 
   if (!userId) {
     throw new AuthenticationError('You are not logged in');
   }
 
-  req.user = { _id: userId, role: userRole };
+  req.user = { _id: getObjId(userId), role: userRole };
 
   next();
 };
 
 export const authorize =
   (allowedRoles: TUserRole[]) => (req: Request, _res: Response, next: NextFunction) => {
-    const userId = req.get(HEADERS_MAP.userId) ?? '';
+    const userId = req.get(HEADERS_MAP.userId);
 
     if (!userId) {
       throw new AuthenticationError('You are not logged in');
@@ -30,7 +31,7 @@ export const authorize =
       throw new AuthorizationError('You are not authorized to perform this action');
     }
 
-    req.user = { _id: userId, role: userRole };
+    req.user = { _id: getObjId(userId), role: userRole };
 
     next();
   };
