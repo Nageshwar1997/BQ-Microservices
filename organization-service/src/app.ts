7013fa06a4e5f8ebf147a7102cb5189b70edd679
Nageshwar <nageshpawarpatil@gmail.com>
2026-07-31 01:55:28 +0530

    service name updated

--- organization-service/src/app.ts
+++ organization-service/src/app.ts
@@ -77,7 +77,7 @@ app[health.method](health.path, (_, res) => {
     message: 'Organization Service is healthy',
     data: {
       database: getConnectionHealth(),
-      service: SERVICE_NAMES_MAP['user-service'],
+      service: SERVICE_NAMES_MAP['organization-service'],
     },
   });
 });
